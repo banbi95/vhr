11f12cac2221dd666ae31468aee850bea2d950d3
Devin <1790236881@qq.com>
2025-03-27 16:07:14 +0800

    try fixing session issue

--- vhr/vhrserver/vhr-web/src/main/java/org/javaboy/vhr/VhrApplication.java
+++ vhr/vhrserver/vhr-web/src/main/java/org/javaboy/vhr/VhrApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableCaching
 @MapperScan(basePackages = "org.javaboy.vhr.mapper")
 @EnableScheduling
-@enableRedisHttpSession
+@EnableRedisHttpSession
 public class VhrApplication {
 
     public static void main(String[] args) {
